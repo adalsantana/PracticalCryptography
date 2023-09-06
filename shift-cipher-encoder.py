01bda8db490433db97bccd923d5fa4eee6e6bf01
Adal <50596470+adalsantana@users.noreply.github.com>
2023-09-06 14:49:17 -0700

    attempted exercise 1.2 and 1.3. Needs testing
    Want to complete exercise 1.4 asking how many substitution dictionaries are possible for the cryptogram style substitution from the previous exercises but I am not sure what that means... do I do some kind of permutation or combination from statistics and report a number because there is a limited number of possibilities as you map each letter?

--- shift-cipher-encoder.py
+++ shift-cipher-encoder.py
@@ -1,5 +1,6 @@
 import string
-
+import random 
+# DON'T UNDERSTAND WHAT EXERCISE 1.4 MEANS
 def create_shift_substitution(n): 
     encoding = {}
     decoding = {}
@@ -11,6 +12,21 @@ def create_shift_substitution(n):
         encoding[letter] = subst_letter
         decoding[subst_letter] = letter
     return encoding, decoding
+
+# Exercise 1,3 from the book
+# Test for correctness 
+def create_random_substitution():
+    encoding = {}
+    decoding = {}
+    alphabet_size = len(string.ascii_uppercase)
+    alphabet = string.ascii_uppercase
+    key = random.shuffle(alphabet)
+    for i in range(alphabet_size):
+        letter = string.ascii_uppercase[i]
+        subst_letter = key[i]
+        encoding[letter] = subst_letter
+        decoding[subst_letter] = letter
+    return encoding, decoding 
 
 def encode(message, subst): 
     cipher = ""
@@ -25,6 +41,40 @@ def encode(message, subst):
 
 def decode(message, subst): 
     return encode(message, subst)
+
+# Generate every possible shift cipher for a message 
+def generate_all_shift_ciphers(msg):
+    alphabet_size = len(string.ascii_uppercase)
+    decode_list = []
+    for i in range(1, alphabet_size):
+        encoding, decoding = create_shift_substitution(i);
+        decode_attempt = decode(msg, decoding)
+        print("{}. {}".format(i, decode_attempt)) #store the values for comparing later?
+        decode_list.append(decode_attempt)
+    return decode_list
+# To Do: Refine how you count word occurences. Test for correctness.
+# This function implements exercise 1.2 Automated Decoding
+def auto_decode(msg, eng_dict):
+    # STILL NEEDED: SOURCE DICTIONARY
+    alphabet_size = len(string.ascii_uppercase)
+    hit_counter = 0
+    # format: ("decoded message", hitCount, cipherShift)
+    maxHits = ("placeholder", -1, -1) 
+    for cipherShift in range(1, alphabet_size):
+        encoding, decoding = create_shift_substitution(cipherShift);
+        decode_attempt = decode(msg, decoding)
+        hit_counter = 0
+        print("\nShift Value: {} Decoded Value: {}".format(cipherShift, decode_attempt)) #store the values for comparing later?
+        for word in eng_dict:  
+            if word in decode_attempt: 
+                hit_counter += 1
+        if hit_counter[decode_attempt] > maxHits[1]:
+            maxHits = (decode_attempt, hit_counter, cipherShift)
+
+    return maxHits
+
+     
+
 
 def printable_substitution(subst):
     # sort by source character so things are alphabetized
