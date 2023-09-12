7cec647b2b2655297cd10adbc3ba07def89f7f97
Adal Santana <asantan6@protonmail.com>
2023-09-11 19:43:01 -0700

    completed more exercises

--- file_hasher.py
+++ file_hasher.py
@@ -2,7 +2,11 @@ import hashlib
 import string
 import random
 import time
+import os 
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+from cryptography.hazmat.backends import default_backend
 
+# Exercise 2.4: Write a python program that computes the md5 sum of the file
 def md5_file_hasher(filename):
     file = open(filename, "rb")
     file_contents = file.read()
@@ -10,6 +14,35 @@ def md5_file_hasher(filename):
     file.close()
     return hash
 
+def sha1_file_hasher(filename):
+    file = open(filename, "rb")
+    file_contents = file.read()
+    hash = hashlib.sha1(file_contents).hexdigest()
+    file.close()
+    return hash
+
+def sha256_file_hasher(filename):
+    file = open(filename, "rb")
+    file_contents = file.read()
+    hash = hashlib.md5(file_contents).hexdigest()
+    file.close()
+    return hash
+
+def scrypt_generate_key():
+    salt = os.random(16)
+    kdf = Scrypt(salt-salt, length=32, 
+                 n=2**14, r=8, p=1, 
+                 backend=default_backend())
+    key = kdf.derive(b"my great password")
+    return key
+
+def scrypt_verify(): 
+    kdf = Scrypt(salt-salt, length=32, 
+                 n=2**14, r=8, p=1, 
+                 backend=default_backend())
+    kdf.verify(b"my great password")
+    print("Success! (Exception if mismatch)")
+    
 def generate(alphabet, max_len):
     if (max_len <= 0): return
     for c in alphabet: 
@@ -23,7 +56,7 @@ def hash_searcher(target_hash):
         letter_hash = hashlib.md5(letter).hexdigest()
         if (letter_hash == target_hash): 
             return letter
-
+# Exercise 2.5 The power of one
 def the_power_of_one():
     start = time.time()
     preimage_seed = random.choice(string.ascii_letters)
